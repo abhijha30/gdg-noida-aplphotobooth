1ee6f18afbaa42aa62e53b8d74be4af99b85b00b
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-05-31 08:56:11 +0530

    Update CameraView.tsx

--- src/components/CameraView.tsx
+++ src/components/CameraView.tsx
@@ -53,7 +53,7 @@ export const CameraView = forwardRef<CameraViewHandle, CameraViewProps>(
     useEffect(() => {
       const img = new Image();
       img.crossOrigin = "anonymous";
-      img.src = "/jersey.jpeg";
+      img.src = "/jersey.jpeg.png";
       img.onload = () => {
         jerseyImgRef.current = img;
       };
