69e3a4932e059acc38c964afa6446ce5a9b8d0ef
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-05-31 12:39:09 +0530

    Update PhotoBooth.tsx

--- src/components/PhotoBooth.tsx
+++ src/components/PhotoBooth.tsx
@@ -44,7 +44,7 @@ export default function PhotoBooth() {
   useEffect(() => {
     const img = new Image();
     img.crossOrigin = "anonymous";
-    img.src = "/jersey-overlay.png";
+    img.src = "/gdg-overlay-v2.png";
     img.onload = () => {
       jerseyImgRef.current = img;
     };
