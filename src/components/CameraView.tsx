0f65f4f42feb4594b98f3c7345ae54644af1853a
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-05-31 12:38:42 +0530

    Update CameraView.tsx

--- src/components/CameraView.tsx
+++ src/components/CameraView.tsx
@@ -53,7 +53,7 @@ export const CameraView = forwardRef<CameraViewHandle, CameraViewProps>(
     useEffect(() => {
       const img = new Image();
       img.crossOrigin = "anonymous";
-      img.src = "/jersey-overlay.png";
+      img.src = "/gdg-overlay-v2.png";
       img.onload = () => { jerseyImgRef.current = img; };
     }, []);
 
